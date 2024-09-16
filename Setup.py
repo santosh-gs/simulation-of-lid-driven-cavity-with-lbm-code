fea78e7a2edf4c0a0792d13a2a3e7bc2fcb7beb6
Santosh GS <santoshgs710@gmail.com>
2024-09-16 18:24:07 +0530

    Add Setup.py

---
+++ Setup.py
@@ -0,0 +1,15 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+import numpy
+
+extensions = [Extension(
+                "LBM_Cython",
+                sources=["LBM_Cython.pyx"],
+                extra_compile_args=["/openmp"],
+                extra_link_args=["/openmp"]
+            )]
+
+setup(
+    ext_modules = cythonize(extensions, compiler_directives={'boundscheck': False, 'cdivision': False}, annotate = True, language_level = 3), include_dirs=[numpy.get_include()]
+)
